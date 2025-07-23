c5847fc341a2083a167c63828851728488a4d951
stevbrown <stevbrowndev@gmail.com>
2025-07-23 20:28:32 +0500

    23.07.2025

--- script/corrector.js
+++ script/corrector.js
@@ -1,3 +1,5 @@
+import { createAlert } from "./alert.js";
+
 function phoneCorrector() {
     var input = document.getElementById('phone-number');
 
@@ -50,3 +52,56 @@ function codeCorrector() {
         this.setSelectionRange(start, end);
     });
 }
+
+function blockCyrillic(event) {
+    const input = event.target;
+    const cyrillicPattern = /[А-Яа-яЁё]/;
+
+    if (cyrillicPattern.test(input.value)) {
+        input.value = input.value.replace(cyrillicPattern, '');
+        createAlert(`You are using invalid characters!`, 'warning');
+    }
+}
+
+function mailCorrector(event) {
+    const input = event.target;
+    const forbiddenPattern = /[ \t!#$%&'()*+,/:;<=>?[\]~]/;
+
+    blockCyrillic(event);
+
+    if (forbiddenPattern.test(input.value)) {
+        input.value = input.value.replace(forbiddenPattern, '');
+        createAlert(`You are using invalid characters!`, 'warning');
+    }
+
+    input.value = input.value.toLowerCase();
+
+    const cleanedValue = input.value
+        .replace(/\.{2,}/g, '.') 
+        .replace(/^\./, '')
+    input.value = cleanedValue;
+}
+
+function mailCorrectCheck(event) {
+    const input = event.target;
+
+    if (input.value.length < 5) {
+        createAlert('The provided email address is too short and lacks the necessary components to be considered valid.', 'error');
+        return;
+    }
+
+    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+
+    if (!emailPattern.test(input.value)) {
+        createAlert('The entered email address is invalid and does not meet the required format.', 'error');
+    } 
+}
+
+
+window.phoneCorrector = phoneCorrector;
+window.formatPhoneNumber = formatPhoneNumber;
+window.phoneInputClear = phoneInputClear;
+window.codeCorrector = codeCorrector;
+window.blockCyrillic = blockCyrillic;
+window.mailCorrector = mailCorrector;
+window.mailCorrectCheck = mailCorrectCheck;
--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@ import { getEmojiFlags } from "./getEmoji.js";
 import { createAlert } from "./alert.js";
 
 var code;
+var number_conf;
 
 document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.remove("unloaded");
@@ -46,9 +47,9 @@ document.querySelector('#sendCode').addEventListener('click', function() {
     try {
         const phoneInput = document.querySelector('#phone-number');
         const phoneNumber = phoneInput ? phoneInput.value.replace(/\D/g, '') : '';
-        const phoneCode = document.querySelector('#phone-code');
+        const phoneCode = document.querySelector('#phone-code').value;
 
-        const code = phoneCode.value;
+        // const code = phoneCode.value;
         const number = phoneInput.value.replace(/\s+/g, '');
 
         const step1 = document.getElementById('step1');
@@ -56,15 +57,15 @@ document.querySelector('#sendCode').addEventListener('click', function() {
         const progress = document.getElementById('progress-bar');
         const bar = document.querySelector('.progress-bar');
 
-        if(!phoneNumber || !phoneCode.value || !phoneInput) {
+        if(!phoneNumber || !phoneCode || !phoneInput) {
             createAlert('Oops! We couldn’t proceed without your phone number. Please enter your phone number to continue.', "error");
         } else if (phoneNumber.length < 10) {
             createAlert('The phone number you entered is incorrect or not in the correct format. Please check and try again.', "warning");
         } else {
-            createAlert(`Your verification code has been successfully sent to +${code}${number}. We have sent an SMS with a verification code to the number you provided. Please check your messages to proceed.`, 'success');
+            createAlert(`Your verification code has been successfully sent to +${phoneCode}${number}. We have sent an SMS with a verification code to the number you provided. Please check your messages to proceed.`, 'success');
             bar.classList.remove('active');
             setTimeout(() => {
-                progress.src = '/img/progress_bar/SecondCheck.svg'
+                progress.src = './img/progress_bar/SecondCheck.svg'
                 bar.classList.add('active');
             }, 550);
 
@@ -77,7 +78,8 @@ document.querySelector('#sendCode').addEventListener('click', function() {
                     step2.classList.add('active');
                 }, 25)
             }, 1300);
-            pushCode(`+${code} ${phoneInput.value}`);
+            pushCode(`+${phoneCode} ${phoneInput.value}`);
+            number_conf = `+${phoneCode} ${phoneInput.value}`;
         }
     } catch(error) {
         alert(`Error ${error}`);
@@ -87,6 +89,12 @@ document.querySelector('#sendCode').addEventListener('click', function() {
 document.querySelector('#confirmCode').addEventListener('click', function() {
     try {
         const codeInput = document.getElementById('code-enter').value;
+        const progress = document.getElementById('progress-bar');
+        const bar = document.querySelector('.progress-bar');
+        const step2 = document.getElementById('step2');
+        const step3 = document.getElementById('step3');
+
+        const confirmedNumber = document.getElementById('confirmed-number');
 
         if (!codeInput || codeInput.length < 4) {
             createAlert('Attention! The verification code cannot be empty or incorrect.', 'error');
@@ -94,6 +102,23 @@ document.querySelector('#confirmCode').addEventListener('click', function() {
             createAlert('The verification code you entered does not match the code sent to your phone.', 'warning')
         } else {
             createAlert('Congratulations! Your verification code is correct.', 'success');
+
+            bar.classList.remove('active');
+            setTimeout(() => {
+                progress.src = './img/progress_bar/ThirdCheck.svg'
+                bar.classList.add('active');
+            }, 550);
+
+            step2.classList.remove('active');
+            step2.classList.add('disactive');
+            setTimeout(() => {
+                step2.style.display = 'none';
+                step3.style.display = 'flex';
+                setTimeout(() => {
+                    step3.classList.add('active');
+                }, 25)
+            }, 1300);
+            confirmedNumber.textContent = `${number_conf}`
         }
     } catch (error) {
         alert(`Error ${error}`);
@@ -133,7 +158,90 @@ function generateRandomCode() {
     return Math.floor(1000 + Math.random() * 9000);
 }
 
+function setActive(step) {
+    const step1 = document.getElementById('step1');
+    const step2 = document.getElementById('step2');
+    const step3 = document.getElementById('step3');
+
+    step1.classList.remove('active');
+    step2.classList.remove('active');
+    step3.classList.remove('active');
+
+    step1.style.display = 'none';
+    step2.style.display = 'none';
+    step3.style.display = 'none';
+
+
+    switch (step) {
+        case 1:
+            step1.style.display = 'flex';
+            step1.classList.add('active');
+            break;
+
+        case 2:
+            step2.style.display = 'flex';
+            step2.classList.add('active');
+            break;  
+            
+        case 3:
+            step3.style.display = 'flex';
+            step3.classList.add('active');
+            break;
+    
+        default:
+            break;
+    }
+}
+
+document.querySelector('#showPass').addEventListener('click', function() {
+    try {
+        const element = document.getElementById('pass-word');
+        const img = document.getElementById('showPass__img');
+
+        if (element.getAttribute('type') === 'password') {
+            element.type = 'text';
+            img.src = './img/EyeSlash.svg';
+        } else if (element.getAttribute('type') === 'text') {
+            element.type = 'password';
+            img.src = './img/OpenEye.svg';
+        }
+    } catch (error) {
+        alert(`Error ${error}`);
+    }
+});
+
+function checkPassword() {
+    const element = document.getElementById('pass-word');
+    const caption = document.getElementById('caption__status');
+    const imgstatus = document.getElementById('caption-img__status');
+    const status = document.getElementById('caption-text__status');
+
+    if (element.value.length != 0 && element.value.length > 0) {
+        caption.style.display = 'flex';
+
+        imgstatus.src = './img/Prohibit.svg';
+        status.textContent = 'Bad password';
+        status.style.color = '#E94747';
+    } else caption.style.display = 'none';
+
+    if (element.value.length >= 6) {
+        imgstatus.src = './img/WarningSm.svg';
+        status.innerText = 'Weak password';
+        status.style.color = '#FFC506';
+    }
+
+    if (element.value.length >= 12) {
+        imgstatus.src = './img/Mark.svg';
+        status.innerText = 'Good password';
+        status.style.color = '#34C759';
+    }
+}
+
 window.getEmojiFlags = getEmojiFlags;
 window.closeNotify = closeNotify;
 window.closePage = closePage;
 window.resendCode = resendCode;
+window.checkPassword =  checkPassword;
+
+
+window.setActive = setActive;
